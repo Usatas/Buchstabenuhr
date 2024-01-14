6df806151daa6d67e302650095d6f04aa8784057
Usatas <aust_frederic@web.de>
2024-01-14 22:15:28 +0100

    Die Main wurde zum initialen kalibrieren der RTC und debugging des Zusammenspiels aus NetworkHandler und RTCHandler umgebaut. Zudem wurde das LED Define schon mal auf 3*112 angepasst, um dem aktuellen Bauplan zu entsprechen. Ein finaler Plan für den Definitionsort der Defaultconfig steht noch aus.

--- Pi Pico W/main.py
+++ Pi Pico W/main.py
@@ -11,8 +11,15 @@ from Buchstabenuhr import Buchstabenuhr
 from LEDHandler import LEDHandler
 
 LEDPIN = 25
-AMOUNT_LEDS = 112*3 # 2 per letter and one (skipped) for space
+AMOUNT_LEDS = 112*3 # (108 letter + 4 hearts) * 2 LEDs per letter and one (skipped) for space
 
+WLAN_DEFAUT = {
+    "wlan_ssid": "Buchstabenuhr",
+    "wlan_password": "Buchstabenuhr",
+    "wlan_mode": "host"
+}
+
+import time
 def main():
     print("main")
     # TODO Load config from file
@@ -20,6 +27,13 @@ def main():
     config_handler.load_config_from_file()
     # TODO Connect to network
     network_handler = NetworkHandler(config_handler)
+    is_connected = network_handler.connect_to_wlan()
+    print(f"Connected to WLAN: {is_connected}")
+    if not is_connected:
+        print("Not connected to WLAN - wait 10s and try again")
+        time.sleep(10)
+        is_connected =network_handler.connect_to_wlan()
+        print(f"Connected to WLAN: {is_connected}")
     # TODO Initialize RCT
     rtc_handler = RTCHandler()
     led_handler = LEDHandler(LEDPIN, AMOUNT_LEDS)
@@ -27,11 +41,46 @@ def main():
     uhr = Buchstabenuhr(config_handler, network_handler, rtc_handler,led_handler)
     # TODO run Buchstabenuhr
     try:
-        uhr.calibrate_rtc()
-        uhr.run()
+       print("Start main try")
+
+       print(network_handler.wlan.isconnected())
+       if network_handler.wlan.isconnected():
+        print("Connected to WLAN - load time from network")
+        network_time = network_handler.request_current_time("Europe/Berlin")
+        print (f"network_time: {network_time}")
+        if network_time is not None:
+            print("Calibrate RTC")
+            rtc_handler.calibrate_rtc(network_time)
+        else:
+            print("No network time available")
+
+        for i in range(30):
+            time2 = rtc_handler.DS3231_ReadTime(0)
+            print(time2)
+            time.sleep(1)
+        # uhr.run()
     except:
         print("Exception while running Buchstabenuhr")
         # TODO Add handle of exceptions .. maybe blinking for 10s and restart 
+
+    finally:
+        network_handler.wlan.disconnect()
+        print("Disconnected from WLAN")
+        print(network_handler.wlan.status())
+        print(network_handler.wlan.isconnected())
+        print(network_handler.wlan.ifconfig())
+        print("End main try")
+
+
+def test2():
+    print("Test Main")
+    rtc2 = RTCHandler()
+    time2 = rtc2.DS3231_ReadTime(0)
+    print(time2)
+    for i in range(10):
+        time2 = rtc2.DS3231_ReadTime(0)
+        print(time2)
+        time.sleep(10)
 
 if __name__ == "__main__":
     main()
